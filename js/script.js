917c7c4a2c52c0aef3d5cc0eaf52f03065e284a6
Sakibur59 <sakiburrahman5978@gmail.com>
2026-02-22 17:22:03 +0600

    Toggle part added and find the all card innertext

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let interviewList = [];
+let rejectedList = [];
 let total = document.getElementById("total-job");
 
 let interview = document.getElementById("interview-count");
@@ -10,6 +12,51 @@ let availableJob= document.getElementById("available-job");
 function count(){
     total.innerText = allCard.children.length;
     availableJob.innerText = allCard.children.length;
+    interview.innerText = interviewList.length;
+    rejected.innerText = rejectedList.length;
 }
 
 count();
+
+let allBtn = document.getElementById("all-btn");
+let interviewBtn = document.getElementById("interview-btn");
+let rejectedBtn = document.getElementById("rejected-btn");
+
+function toggle(id){
+    allBtn.classList.remove('bg-[#3B82F6]','text-white')
+    interviewBtn.classList.remove('bg-[#3B82F6]','text-white')
+    rejectedBtn.classList.remove('bg-[#3B82F6]','text-white')
+
+    allBtn.classList.add('bg-white','text-[#64748B]','border-gray-300')
+    interviewBtn.classList.add('bg-white','text-[#64748B]','border-gray-300')
+    rejectedBtn.classList.add('bg-white','text-[#64748B]','border-gray-300')
+
+    let selected = document.getElementById(id);
+    selected.classList.remove('bg-white','text-[#64748B]','border-gray-300');
+    selected.classList.add('bg-[#3B82F6]','text-white')
+}
+
+let main = document.querySelector('main');
+
+main.addEventListener('click',function (event){
+    let parentNode = event.target.parentNode.parentNode;
+
+    let name = parentNode.querySelector('.name').innerText ;
+    let role = parentNode.querySelector('.role').innerText ;
+    let salary = parentNode.querySelector('.salary').innerText ;
+    let result = parentNode.querySelector('.result').innerText ;
+    let details = parentNode.querySelector('.details').innerText ;
+    let int_btn = parentNode.querySelector('.int-btn').innerText ;
+    let rej_btn = parentNode.querySelector('.rej-btn').innerText ;
+
+    const cardInfo={
+        name,
+        role,
+        salary,
+        result,
+        details,
+        int_btn,
+        rej_btn
+    }
+    
+})
